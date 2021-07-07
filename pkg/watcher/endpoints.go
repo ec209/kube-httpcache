a2c87f562bd14ae9190719786686cda844161681
TAEGI <felimox.kim@gmail.com>
2021-07-06 20:08:27 -0400

    add some comments

--- pkg/watcher/endpoints.go
+++ pkg/watcher/endpoints.go
@@ -24,18 +24,27 @@ type Endpoint struct {
 
 type EndpointList []Endpoint
 
+// Checks if the endpoint from receiver and parameter is the same
 func (l EndpointList) EqualsEndpoints(ep v1.EndpointSubset) bool {
+	// if the length of address is different, it cannot be the same endpoint
 	if len(l) != len(ep.Addresses) {
 		return false
 	}
 
+	// Declare a map var with string key and bool value
 	matchingAddresses := map[string]bool{}
+	// iterate EndpointList l
 	for i := range l {
+		// Not sure why it cannot be i.Host
+		// Create a hashmap for Hosts
 		matchingAddresses[l[i].Host] = true
 	}
 
+	// iterrate endpoints from k8s API (Not sure)
 	for i := range ep.Addresses {
+		// h is the IP of endpoint
 		h := ep.Addresses[i].IP
+		// find if that IP(Host) from the hashmap created in the previous for loop. This will show if l and ep shares the same host or not
 		_, ok := matchingAddresses[h]
 		if !ok {
 			return false
@@ -51,6 +60,7 @@ func (l EndpointList) Contains(b *Endpoint) bool {
 	}
 
 	for i := range l {
+		// if the host and port of l and b are the same
 		if l[i].Host == b.Host && l[i].Port == b.Port {
 			return true
 		}
@@ -62,9 +72,11 @@ func (l EndpointList) Contains(b *Endpoint) bool {
 func EndpointListFromSubset(ep v1.EndpointSubset, portName string) (EndpointList, error) {
 	var port int32
 
+	// make an Endpotlist list with the size of ep
 	l := make(EndpointList, len(ep.Addresses))
 
 	for i := range ep.Ports {
+		// find a port based on the portName parameter
 		if ep.Ports[i].Name == portName {
 			port = ep.Ports[i].Port
 		}
@@ -77,10 +89,12 @@ func EndpointListFromSubset(ep v1.EndpointSubset, portName string) (EndpointList
 	for i := range ep.Addresses {
 		a := &ep.Addresses[i]
 
+		// fill up name to the list l
 		if a.TargetRef != nil {
 			l[i].Name = a.TargetRef.Name
 		}
 
+		// fill up the IP and port to the list l
 		l[i].Host = a.IP
 		l[i].Port = strconv.Itoa(int(port))
 	}
