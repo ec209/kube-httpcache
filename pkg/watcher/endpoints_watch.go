9f0e90fdaa5999b33b731879d2614acce59c857c
TAEGI <felimox.kim@gmail.com>
2021-07-06 21:31:12 -0400

    add some comments

--- pkg/watcher/endpoints_watch.go
+++ pkg/watcher/endpoints_watch.go
@@ -10,6 +10,7 @@ import (
 	"k8s.io/apimachinery/pkg/watch"
 )
 
+// start a go routine with method watch. in the end returns two channels
 func (v *EndpointWatcher) Run() (chan *EndpointConfig, chan error) {
 	updates := make(chan *EndpointConfig)
 	errors := make(chan error)
@@ -20,7 +21,9 @@ func (v *EndpointWatcher) Run() (chan *EndpointConfig, chan error) {
 }
 
 func (v *EndpointWatcher) watch(updates chan *EndpointConfig, errors chan error) {
+	// indefinte for loop
 	for {
+		// watch k8s endpoint in certain namespace. with certain service name field.
 		w, err := v.client.CoreV1().Endpoints(v.namespace).Watch(metav1.ListOptions{
 			FieldSelector: fields.OneTermEqualSelector("metadata.name", v.serviceName).String(),
 		})
@@ -33,19 +36,24 @@ func (v *EndpointWatcher) watch(updates chan *EndpointConfig, errors chan error)
 			continue
 		}
 
+		// create a channel, watch the input from w
 		c := w.ResultChan()
 		for ev := range c {
+			// ev.Type shows the status of watch(not clear)
 			if ev.Type == watch.Error {
 				glog.Warningf("error while watching: %+v", ev.Object)
 				continue
 			}
 
+			// if nothing has added or modified, skip the loop
 			if ev.Type != watch.Added && ev.Type != watch.Modified {
 				continue
 			}
 
 			endpoint := ev.Object.(*v1.Endpoints)
 
+			// if the length of endpoint is o or if there is no address in the endpoint object,
+			// shows the warning message and construct a new endpoint and assing it to v(receiver)
 			if len(endpoint.Subsets) == 0 || len(endpoint.Subsets[0].Addresses) == 0 {
 				glog.Warningf("service '%s' has no endpoints", v.serviceName)
 
@@ -54,6 +62,7 @@ func (v *EndpointWatcher) watch(updates chan *EndpointConfig, errors chan error)
 				continue
 			}
 
+			// v is the current state, endpoint would be the previous state of endpoint
 			if v.endpointConfig.Endpoints.EqualsEndpoints(endpoint.Subsets[0]) {
 				glog.Infof("endpoints did not change")
 				continue
@@ -63,6 +72,7 @@ func (v *EndpointWatcher) watch(updates chan *EndpointConfig, errors chan error)
 			for _, a := range endpoint.Subsets[0].Addresses {
 				puid := string(a.TargetRef.UID)
 
+				// Get the pod object
 				po, err := v.client.CoreV1().Pods(v.namespace).Get(a.TargetRef.Name, metav1.GetOptions{})
 
 				if err != nil {
@@ -75,25 +85,30 @@ func (v *EndpointWatcher) watch(updates chan *EndpointConfig, errors chan error)
 					continue
 				}
 
+				// append endpoint address a to the list addresses
 				addresses = append(addresses, a)
 			}
 
+			// if there nothing in the addresses list, construct a new endpoint using NewEndpointConfig() function
 			if len(addresses) == 0 {
 				glog.Warningf("service '%s' has no endpoint that is ready", v.serviceName)
 				v.endpointConfig = NewEndpointConfig()
 				continue
 			}
 
+			// Not sure why assigning addresses back to the endpoint
 			endpoint.Subsets[0].Addresses = addresses
 
 			newConfig := NewEndpointConfig()
 
+			// not sure why is it a BackendList, possibly due the the things been done to the endpoint variable in advance
 			newBackendList, err := EndpointListFromSubset(endpoint.Subsets[0], v.portName)
 			if err != nil {
 				glog.Errorf("error while building backend list: %s", err.Error())
 				continue
 			}
 
+			// Not sure what is this Primary variable is doing
 			if v.endpointConfig.Primary != nil && newBackendList.Contains(v.endpointConfig.Primary) {
 				newConfig.Primary = v.endpointConfig.Primary
 			} else {
