7971d4f09abd1204f0cc9d0cae1a700224a52a17
TAEGI <felimox.kim@gmail.com>
2021-07-08 08:51:35 -0400

    add some comments

--- pkg/controller/run.go
+++ pkg/controller/run.go
@@ -14,25 +14,26 @@ import (
 func (v *VarnishController) Run(ctx context.Context) error {
 	glog.Infof("waiting for initial configuration before starting Varnish")
 
-	v.frontend = watcher.NewEndpointConfig()
-	if v.frontendUpdates != nil {
-		v.frontend = <-v.frontendUpdates
+	v.frontend = watcher.NewEndpointConfig() // assign an empty endpoint to frontend of varnish controller
+	if v.frontendUpdates != nil {            // the watcher function keep it's eye on endpoint channel, if there is a change
+		v.frontend = <-v.frontendUpdates // update the frontend with the item from the channel
 		if v.varnishSignaller != nil {
-			v.varnishSignaller.SetEndpoints(v.frontend)
+			v.varnishSignaller.SetEndpoints(v.frontend) // update signaller's endpoint
 		}
 	}
 
 	v.backend = watcher.NewEndpointConfig()
 	if v.backendUpdates != nil {
-		v.backend = <-v.backendUpdates
+		v.backend = <-v.backendUpdates // update backend endconfig
 	}
 
-	target, err := os.Create(v.configFile)
+	target, err := os.Create(v.configFile) // bring up configFile
 	if err != nil {
 		return err
 	}
 
 	glog.Infof("creating initial VCL config")
+	// Write Endpoints, Primary Endpoint, backend_endpoints, and Primary Backend_endpoint to target
 	err = v.renderVCL(target, v.frontend.Endpoints, v.frontend.Primary, v.backend.Endpoints, v.backend.Primary)
 	if err != nil {
 		return err
@@ -45,8 +46,9 @@ func (v *VarnishController) Run(ctx context.Context) error {
 	}
 
 	watchErrors := make(chan error)
-	go v.watchConfigUpdates(ctx, cmd, watchErrors)
+	go v.watchConfigUpdates(ctx, cmd, watchErrors) // this go routine watches for the frontend/backend/template update or error our if the ctx got cancelled
 
+	// This go routine basically prints out the logs from the go routine above
 	go func() {
 		for err := range watchErrors {
 			if err != nil {
@@ -55,22 +57,25 @@ func (v *VarnishController) Run(ctx context.Context) error {
 		}
 	}()
 
-	return <-errChan
+	return <-errChan // channel about error from varnishd cmd
 }
 
 func (v *VarnishController) startVarnish(ctx context.Context) (*exec.Cmd, <-chan error) {
+	// inject certain cmd to the context
 	c := exec.CommandContext(
 		ctx,
 		"varnishd",
-		v.generateArgs()...,
+		v.generateArgs()..., // This ... prolly refers that there are multiple outputs
 	)
 
+	// default dir
 	c.Dir = "/"
 	c.Stdout = os.Stdout
 	c.Stderr = os.Stderr
 
 	r := make(chan error)
 
+	// this go routine actually runs Commands defined in CommandContext
 	go func() {
 		err := c.Run()
 		r <- err
